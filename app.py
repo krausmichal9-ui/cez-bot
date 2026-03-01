9f85e365286f20278da1d53234fa23d71a85a709
krausmichal9-ui <krausmichal9@gmail.com>
2026-03-01 20:21:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -192,4 +192,6 @@ async def kresli_polygon(page, presah_px):
 if __name__ == "__main__":
 import os
 port = int(os.environ.get("PORT", 5000))
+import os
+port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port, debug=False)
